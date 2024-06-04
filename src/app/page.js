9bf36e0d06c8713cc25144f5c61663a9abb48de3
Anupama Singh <anupama12sg@gmail.com>
2024-06-04 21:35:58 +0530

    Changed the color of my name, added paras

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="m-12">
-      
-    <p className="text-3xl underline underline-offset-8"><span className="italic">Hello!</span> It's me, Anu.</p> 
-    
 
-
-
+      <p className="text-3xl underline underline-offset-8"><span className="italic">Hello!</span> It's me, <span class="text-orange-400">Anu</span>.</p>
+      <br></br>
+      <p><span className="text-purple-400">Welcome </span> to this little corner of my world.</p>
+      <br></br>
+      <p>I'm a Assistant Professor and an aspiring Software Dev. Teaching holds a special place in my heart, but my true passion lies in coding, crafting technical articles and an ardent reader, a wordsmith in the making. My dream is to fill my days with the joy of coding, tackling real-life challenges via code and sharing my knowledge through written insights.  </p>
+      <br></br>
+      <p>My interests span across Software Development, Web2, Web3, Full-Stack Development, and my eclectic hobbies include exploring International Relations, indulging in culinary delights and expressing myself through the art of painting.</p>
     </div>
   );
 }
