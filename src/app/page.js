59c235ca58cc52672a923919e6e13381d04fff28
Anupama Singh <anupama12sg@gmail.com>
2024-06-09 20:51:05 +0530

    Added links to my twitter, github, blog and linkedin.

--- src/app/page.js
+++ src/app/page.js
@@ -8,9 +8,34 @@ export default function Home() {
       <br></br>
       <p><span className="text-purple-400">Welcome </span> to this little corner of my world.</p>
       <br></br>
-      <p>I'm a Assistant Professor and an aspiring Software Dev. Teaching holds a special place in my heart, but my true passion lies in coding, crafting technical articles and an ardent reader, a wordsmith in the making. My dream is to fill my days with the joy of coding, tackling real-life challenges via code and sharing my knowledge through written insights.  </p>
+      <p>I'm a <span class="text-blue-400">Software Developer</span>. I like teaching and my true passion lies in coding. I also like crafting technical articles and am an ardent reader, a wordsmith in the making. My dream is to fill my days with the joy of coding, tackling real-life challenges via code and sharing my knowledge through written insights.  </p>
       <br></br>
       <p>My interests span across Software Development, Web2, Web3, Full-Stack Development, and my eclectic hobbies include exploring International Relations, indulging in culinary delights and expressing myself through the art of painting.</p>
+      <div>
+        <br></br>
+            <ul>
+                <li>
+                    <a href="https://x.com/Anu31415" target="_blank" rel="noopener noreferrer">
+                        Twitter
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/anupama1208/" target="_blank" rel="noopener noreferrer">
+                        LinkedIn
+                    </a>
+                </li>
+                <li>
+                    <a href="https://github.com/anupama12sg" target="_blank" rel="noopener noreferrer">
+                        GitHub
+                    </a>
+                </li>
+                <li>
+                    <a href="https://medium.com/@anupama12sg/the-start-of-something-something-great-b3865db86881" target="_blank" rel="noopener noreferrer">
+                        Blog
+                    </a>
+                </li>
+            </ul>
+        </div>
     </div>
   );
 }
