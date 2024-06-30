9de4f4097c196a38168582d4a5bfd7b78946617f
Anupama Singh <anupama12sg@gmail.com>
2024-06-30 13:07:13 +0530

    chore: Made the entire webpage center aligned.

--- src/app/page.js
+++ src/app/page.js
@@ -4,38 +4,38 @@ import Image from "next/image";
 export default function Home() {
     return (
         <div className="m-12">
-<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 32 32%22><text y=%2226%22 font-size=%2226%22>✨</text></svg>"></link>)
-            <p className="text-3xl underline underline-offset-8"><span className="italic">Hello!</span> It is me, <span class="text-orange-400">Anu</span>.</p>
+<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 32 32%22><text y=%2226%22 font-size=%2226%22>✨</text></svg>"></link>
+            <p className="text-3xl underline underline-offset-8 "><center><span className="italic">Hello!</span> It is me, <span class="text-orange-400">Anu</span>.</center></p>
             <br></br>
-            <p><span className="text-purple-400">Welcome </span> to this little corner of my world.</p>
+            <p><center><span className="text-purple-400">Welcome </span> to this little corner of my world.</center></p>
             <br></br>
-            <p>I am a <span class="text-orange-400">Software Developer</span>. <br></br> <br></br> I like teaching and my true passion lies in coding. <br></br> <br></br> I also like crafting technical articles and am an ardent reader, a wordsmith in the making. <br></br><br></br>My dream is to fill my days with the joy of coding, tackling real-life challenges via code and sharing my knowledge through written insights.  </p>
+            <p><center>I am a <span class="text-orange-400">Software Developer</span>. <br></br> <br></br> I like teaching and my true passion lies in coding. <br></br> <br></br> I also like crafting technical articles and am an ardent reader, a wordsmith in the making. <br></br><br></br>My dream is to fill my days with the joy of coding, tackling real-life challenges via code and sharing my knowledge through written insights.</center></p>
             <br></br>
-            <p>My interests span across <span class="text-orange-400">Software (Full-Stack) Development, Web2, Web3, petting cats</span> and my eclectic hobbies include exploring <span class="text-blue-400">International Relations</span>, indulging in culinary delights and expressing myself through the art of painting.</p>
+            <p><center>My interests span across <span class="text-orange-400">Software (Full-Stack) Development, Web2, Web3, petting cats</span> and my eclectic hobbies include exploring <span class="text-blue-400">International Relations</span>, indulging in culinary delights and expressing myself through the art of painting.</center></p>
             <div>
                 <br></br>
                 <ul>
                     <li>
                         <a href="https://x.com/Anu31415" target="_blank" rel="noopener noreferrer">
-                            <u>Twitter</u>
+                            <u><center>Twitter</center></u>
                         </a>
                     </li>
                     <br></br>
                     <li>
                         <a href="https://www.linkedin.com/in/anupama1208/" target="_blank" rel="noopener noreferrer">
-                           <u>LinkedIn</u> 
+                           <u><center>LinkedIn</center></u> 
                         </a>
                     </li>
                     <br></br>
                     <li>
                         <a href="https://github.com/anupama12sg" target="_blank" rel="noopener noreferrer">
-                           <u>GitHub</u> 
+                           <u><center>GitHub</center></u> 
                         </a>
                     </li>
                     <br></br>
                     <li>
                         <a href="https://medium.com/@anupama12sg/the-start-of-something-something-great-b3865db86881" target="_blank" rel="noopener noreferrer">
-                           <u>Blog</u> 
+                           <u><center>My Blog!</center></u> 
                         </a>
                     </li>
                 </ul>
